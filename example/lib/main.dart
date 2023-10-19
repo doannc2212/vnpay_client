87524833740bff18b5e69a40ce062ff09deac930
0x00646f616e6e63 <doannc2212@gmail.com>
2023-10-19 10:40:03 +0700

    fix: example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:vnpay_client/vnpay_client.dart';
 
 void main() {
@@ -16,46 +13,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _vnpayClientPlugin = VnpayClient();
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _vnpayClientPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+  final paymentUrl = 'example';
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+  void _onPaymentSuccess(data) {}
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  void _onPaymentFailure(error) {}
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: OutlinedButton(
+            onPressed: () {
+              showVNPayScreen(
+                context,
+                paymentUrl: paymentUrl,
+                onPaymentSuccess: _onPaymentSuccess,
+                onPaymentError: _onPaymentFailure,
+              );
+            },
+            child: const Text('Click me'),
+          ),
         ),
       ),
     );
